73085f5ae15de783ac96c071dbf548bae1e6bf8d
Dmitrii Losunov <dmitrij.dmitriy32@yandex.ru>
2024-11-03 09:03:22 -0800

    Добавил функцию входа через кнопку Личный кабинет с главной страницы

--- test_login_by_login_account_button.py
+++ test_login_by_login_account_button.py
@@ -4,9 +4,21 @@ from selenium.webdriver.support import expected_conditions
 from locators import StellarBurgersLocators
 
 #заполнение формы входа для проверки регистрации
-def test_successful_login(driver, user_email, user_password):
+def test_successful_login_from_main_page(driver, user_email, user_password):
     driver.get("https://stellarburgers.nomoreparties.site/")
     driver.find_element(*StellarBurgersLocators.LOGIN_BUTTON_MAIN).click()
+    driver.find_element(*StellarBurgersLocators.LOGIN_NAME_FIELD).send_keys(user_email)
+    driver.find_element(*StellarBurgersLocators.PASSWORD_NAME_FIELD).send_keys(user_password)
+    driver.find_element(*StellarBurgersLocators.LOGIN_BUTTON_MAIN_FORM).click()
+    #вход в личный кабинет
+    driver.find_element(*StellarBurgersLocators.ACCOUNT_BUTTON).click()
+    WebDriverWait(driver, 3).until(expected_conditions.visibility_of_element_located((By.XPATH, "//button[text()='Выход']")))
+    assert driver.find_element(By.CLASS_NAME, "Account_button__14Yp3").text == 'Выход'
+
+#заполнение формы входа для проверки регистрации
+def test_successful_login_from_personal_account(driver, user_email, user_password):
+    driver.get("https://stellarburgers.nomoreparties.site/")
+    driver.find_element(*StellarBurgersLocators.ACCOUNT_BUTTON).click()
     driver.find_element(*StellarBurgersLocators.LOGIN_NAME_FIELD).send_keys(user_email)
     driver.find_element(*StellarBurgersLocators.PASSWORD_NAME_FIELD).send_keys(user_password)
     driver.find_element(*StellarBurgersLocators.LOGIN_BUTTON_MAIN_FORM).click()
