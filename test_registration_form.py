35cb73440a5f0bd3722bac7b3a00a6a96b2f236f
Dmitrii Losunov <dmitrij.dmitriy32@yandex.ru>
2024-11-03 08:41:25 -0800

    Обновил функции теста добавил проверку входа после регистрации и почистил код

--- test_registration_form.py
+++ test_registration_form.py
@@ -1,13 +1,9 @@
-import pytest
 from selenium.webdriver.common.by import By
-from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from locators import StellarBurgersLocators
 
-@pytest.mark.usefixtures("user_name", "user_email", "user_password")
 def test_successful_registration(driver, user_name, user_email, user_password):
-    print("Запуск теста на успешную регистрацию")
     #переходим на главную страницу
     driver.get("https://stellarburgers.nomoreparties.site/")
     #нажимаем кнопку войти в аккаунт на главной странице
@@ -21,9 +17,8 @@ def test_successful_registration(driver, user_name, user_email, user_password):
     #нажатие кнопки зарегистрироваться в форме
     driver.find_element(*StellarBurgersLocators.BUTTON_SUBMIT_REGISTRATION).click()
 
-    #заполнение формы входа для проверки регистрации
-@pytest.mark.usefixtures("user_name", "user_email", "user_password")
-def test_successful_login(driver, user_name, user_email, user_password):
+#заполнение формы входа для проверки регистрации
+def test_successful_login(driver, user_email, user_password):
     driver.get("https://stellarburgers.nomoreparties.site/")
     driver.find_element(*StellarBurgersLocators.LOGIN_BUTTON_MAIN).click()
     driver.find_element(*StellarBurgersLocators.LOGIN_NAME_FIELD).send_keys(user_email)
@@ -31,11 +26,25 @@ def test_successful_login(driver, user_name, user_email, user_password):
     driver.find_element(*StellarBurgersLocators.LOGIN_BUTTON_MAIN_FORM).click()
     #вход в личный кабинет
     driver.find_element(*StellarBurgersLocators.ACCOUNT_BUTTON).click()
-
     WebDriverWait(driver, 3).until(expected_conditions.visibility_of_element_located((By.XPATH, "//button[text()='Выход']")))
-
     assert driver.find_element(By.CLASS_NAME, "Account_button__14Yp3").text == 'Выход'
-    print("Элемент 'Выход' найден. Тест пройден.")
+
+def test_registration_with_invalid_password(driver, user_name, user_email, user_password):
+    #переходим на главную страницу
+    driver.get("https://stellarburgers.nomoreparties.site/")
+    #нажимаем кнопку войти в аккаунт на главной странице
+    driver.find_element(*StellarBurgersLocators.LOGIN_BUTTON_MAIN).click()
+    #нажимаем на ссылку зарегистрироваться под формой входа в аккаунт
+    driver.find_element(*StellarBurgersLocators.LINK_TO_REGISTRATION_FORM).click()
+    #заполнение формы регистрации данными
+    driver.find_element(*StellarBurgersLocators.REGISTRATION_NAME_FIELD).send_keys(user_name)
+    driver.find_element(*StellarBurgersLocators.REGISTRATION_EMAIL_FIELD).send_keys(user_email)
+    driver.find_element(*StellarBurgersLocators.REGISTRATION_PASSWORD_FIELD).send_keys("123")
+    #нажатие кнопки зарегистрироваться в форме
+    driver.find_element(*StellarBurgersLocators.BUTTON_SUBMIT_REGISTRATION).click()
+    #проверка отображения сообщения об ошибке
+    assert driver.find_element(By.CLASS_NAME, "input__error").text == 'Некорректный пароль'
+
 
 
 
