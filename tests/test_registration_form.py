e50e27d5abea614d5a162248033328095ac0df4e
Dmitrii Losunov <dmitrij.dmitriy32@yandex.ru>
2024-11-07 22:34:02 -0800

    Добавил файл helpers для генерации данных и проверку после регистрации

--- tests/test_registration_form.py
+++ tests/test_registration_form.py
@@ -2,11 +2,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from locators import StellarBurgersLocators
-from data import URLs, user_name, user_email, user_password
+from data import URLs, user_name, user_email
+from helpers import GenerateCustomData
 
 class TestRegistrationForm:
 
     def test_successful_registration(self, driver):
+        data_generator = GenerateCustomData()
+        user_email = data_generator.generate_custom_emale()
+        user_password = data_generator.generate_custom_password()
         #переходим на главную страницу
         driver.get(URLs.BASE_URL)
          #нажимаем кнопку войти в аккаунт на главной странице
@@ -19,18 +23,8 @@ class TestRegistrationForm:
         driver.find_element(*StellarBurgersLocators.REGISTRATION_PASSWORD_FIELD).send_keys(user_password)
         #нажатие кнопки зарегистрироваться в форме
         driver.find_element(*StellarBurgersLocators.BUTTON_SUBMIT_REGISTRATION).click()
-
-#заполнение формы входа для проверки регистрации
-    def test_successful_login(self, driver):
-        driver.get(URLs.BASE_URL)
-        driver.find_element(*StellarBurgersLocators.LOGIN_BUTTON_MAIN).click()
-        driver.find_element(*StellarBurgersLocators.LOGIN_NAME_FIELD).send_keys(user_email)
-        driver.find_element(*StellarBurgersLocators.PASSWORD_NAME_FIELD).send_keys(user_password)
-        driver.find_element(*StellarBurgersLocators.LOGIN_BUTTON_MAIN_FORM).click()
-        #вход в личный кабинет
-        driver.find_element(*StellarBurgersLocators.ACCOUNT_BUTTON).click()
-        WebDriverWait(driver, 3).until(expected_conditions.visibility_of_element_located((By.XPATH, "//button[text()='Выход']")))
-        assert driver.find_element(By.CLASS_NAME, "Account_button__14Yp3").text == 'Выход'
+        WebDriverWait(driver, 3).until(expected_conditions.visibility_of_element_located((By.XPATH, "//button[text()='Войти']")))
+        assert driver.find_element(By.XPATH, "//button[text()='Войти']").text == 'Войти'
 
     def test_registration_with_invalid_password(self, driver):
         #переходим на главную страницу
@@ -45,6 +39,7 @@ class TestRegistrationForm:
         driver.find_element(*StellarBurgersLocators.REGISTRATION_PASSWORD_FIELD).send_keys("123")
         #нажатие кнопки зарегистрироваться в форме
         driver.find_element(*StellarBurgersLocators.BUTTON_SUBMIT_REGISTRATION).click()
+        WebDriverWait(driver, 3).until(expected_conditions.visibility_of_element_located((By.CLASS_NAME, "input__error")))
         #проверка отображения сообщения об ошибке
         assert driver.find_element(By.CLASS_NAME, "input__error").text == 'Некорректный пароль'
 
